2e52427668b366ad42dca1fda866c7ee4194b32c
Stewart Swearingen <sfswearingen@stewarts-air.lan>
2025-11-14 17:33:55 -0600

    Add console log for sample url

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import app from './app';
 import config from './config/config';
 
 app.listen(config.port, () => {
-  console.log(`Server running at http://localhost:${PORT}/api/forecast/39,-76`);
+  console.log(`Server running on port htt://localhost:${config.port}/api/forecast//39,-76`);
 });
